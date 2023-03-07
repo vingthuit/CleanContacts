edd47e593668ed4e3033b2691d35e3fea9bdb1aa
User <vingt-huit@list.ru>
2023-03-07 12:49:16 +0600

    m

--- app/src/main/java/com/example/cleancontacts/MainActivity.java
+++ app/src/main/java/com/example/cleancontacts/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getContactsWithPermission();
+
+        //button
     }
 
     private void getContactsWithPermission() {
